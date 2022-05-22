0ab98da4dc6f6791b159c28f9b0bc45d4d7dfe8b
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-05-22 01:45:56 -0300

    Lambda e linq

--- Assincrono/Calculo.cs
+++ Assincrono/Calculo.cs
@@ -14,16 +14,16 @@ namespace Assincrono
 
         public Task<List<int>> listaNumeros() {
             var random = new Random(); // numeros aleatorios
-            var list = Enumerable.Range(0, 5).Select(number => random.Next(0, 100)).ToList();
-
+            var list = Enumerable.Range(0, 100).Select(number => random.Next(0, 100)).ToList();
             return Task.Run(() => list);
         }
 
         public async void imprimirNumeros()
         {
             var list = await listaNumeros();
+            var listaFinal = list.Where( list => list <= 10).OrderBy(list => list).Distinct();
             Thread.Sleep(5000);
-            foreach (var l in list)
+            foreach (var l in listaFinal)
             {
                 Console.WriteLine(l.ToString(), ConsoleColor.Blue);
             }
