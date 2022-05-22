59473c6de7b7c5f678b8b8ad600da24463faa709
Welington Marquezini Valverde da Silva <welington_marquezini88@live.com>
2022-05-22 01:21:22 -0300

    Projeto assincrono asp net

---
+++ Assincrono/Calculo.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Assincrono
+{
+    public class Calculo
+    {
+        public Calculo()
+        {
+        }
+
+        public Task<List<int>> listaNumeros() {
+            var random = new Random(); // numeros aleatorios
+            var list = Enumerable.Range(0, 5).Select(number => random.Next(0, 100)).ToList();
+
+            return Task.Run(() => list);
+        }
+
+        public async void imprimirNumeros()
+        {
+            var list = await listaNumeros();
+            Thread.Sleep(5000);
+            foreach (var l in list)
+            {
+                Console.WriteLine(l.ToString(), ConsoleColor.Blue);
+            }
+            Console.WriteLine("--------------------------------------------------", ConsoleColor.Green);
+            Console.WriteLine("Terminei o Trabalho", ConsoleColor.Red);
+        }
+    }
+}
---
+++ Assincrono/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Threading.Tasks;
+
+namespace Assincrono
+{
+    class Program
+    {
+        static async Task Main(string[] args)
+        {
+            try
+            {
+                Calculo c = new Calculo();
+                Console.WriteLine("Chamei o método...", ConsoleColor.Yellow);
+                c.imprimirNumeros();
+                //assicrono
+                //throw new Exception("Erro");
+                Console.WriteLine("Pronto! Tudo deu certo assim que terminar te aviso", ConsoleColor.Green);
+                Console.WriteLine("--------------------------------------------------", ConsoleColor.Green);
+                Console.ReadKey();
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex);
+            } 
+        }
+    }
+}
